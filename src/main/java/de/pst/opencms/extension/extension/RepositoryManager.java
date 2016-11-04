7651fa4b2b204bfad03b8be2b0e97bc946cbe44d
Pascal Strohmeyer <pascal.strohmeyer@comspace.de>
2016-11-04 14:54:05 +0100

    Extracted the Layouts from the manager to the ViewHandler class

--- src/main/java/de/pst/opencms/extension/extension/RepositoryManager.java
+++ src/main/java/de/pst/opencms/extension/extension/RepositoryManager.java
@@ -5,6 +5,7 @@ import de.pst.opencms.extension.listener.FetchClickListener;
 import de.pst.opencms.extension.property.BundleHandler;
 import de.pst.opencms.extension.view.DropdownHandler;
 import de.pst.opencms.extension.view.TableHandler;
+import de.pst.opencms.extension.view.ViewHandler;
 import org.opencms.ui.apps.A_CmsWorkplaceApp;
 
 import java.util.LinkedHashMap;
@@ -12,7 +13,6 @@ import java.util.List;
 
 
 public class RepositoryManager extends A_CmsWorkplaceApp {
-
 
     @Override
     protected LinkedHashMap<String, String> getBreadCrumbForState(String s) {
@@ -23,46 +23,9 @@ public class RepositoryManager extends A_CmsWorkplaceApp {
 
     @Override
     protected Component getComponentForState(String s) {
-        VerticalLayout layout = new VerticalLayout();
-        layout.setSizeUndefined();
-        layout.setMargin(true);
-
-        HorizontalLayout dropDownLayout = new HorizontalLayout();
-        dropDownLayout.setSizeFull();
-
-        DropdownHandler dropdownHandler = new DropdownHandler();
-        ComboBox repoDropdown = dropdownHandler.get();
-
-        Button fetchRepoButton = new Button(BundleHandler.get("button.fetch"));
-        fetchRepoButton.setVisible(true);
-        fetchRepoButton.setWidth("20%");
-
-        dropDownLayout.addComponent(repoDropdown);
-        dropDownLayout.addComponent(fetchRepoButton);
-        dropDownLayout.setComponentAlignment(fetchRepoButton,Alignment.MIDDLE_LEFT);
-
-        dropDownLayout.setExpandRatio(repoDropdown,0.75f);
-        dropDownLayout.setExpandRatio(fetchRepoButton,0.45f);
-
-        layout.addComponent(dropDownLayout);
-
-        TableHandler handler = new TableHandler();
-        Table table = handler.get();
-
-        fetchRepoButton.addClickListener(new FetchClickListener());
-
-        layout.addComponent(table);
-
-        layout.setExpandRatio(dropDownLayout,0.05f);
-        layout.setExpandRatio(table,0.95f);
-
-        layout.setImmediate(true);
-        layout.setVisible(true);
-
-        return layout;
+        ViewHandler viewHandler = new ViewHandler();
+        return viewHandler.getRepositoryLayout();
     }
-
-
 
     @Override
     protected List<NavEntry> getSubNavEntries(String s) {
---
+++ src/main/java/de/pst/opencms/extension/view/ViewHandler.java
@@ -0,0 +1,73 @@
+package de.pst.opencms.extension.view;
+
+import com.vaadin.ui.*;
+import de.pst.opencms.extension.listener.FetchClickListener;
+import de.pst.opencms.extension.property.BundleHandler;
+
+public class ViewHandler {
+    private TableHandler tableHandler;
+    private DropdownHandler dropdownHandler;
+
+
+    public ViewHandler(){
+        this.tableHandler = new TableHandler();
+        this.dropdownHandler = new DropdownHandler();
+    }
+
+    public Component getRepositoryLayout(){
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeUndefined();
+        layout.setMargin(true);
+
+        Component dropdownLayout = getDropdownLayout();
+        layout.addComponent(getDropdownLayout());
+
+        Table table = tableHandler.get();
+        layout.addComponent(table);
+
+        layout.setExpandRatio(dropdownLayout,0.05f);
+        layout.setExpandRatio(table,0.95f);
+
+        layout.setImmediate(true);
+        layout.setVisible(true);
+        return layout;
+    }
+
+    private Component getDropdownLayout(){
+        HorizontalLayout dropDownLayout = new HorizontalLayout();
+        dropDownLayout.setSizeFull();
+
+        ComboBox repoDropdown = dropdownHandler.get();
+
+        Button fetchRepoButton = new Button(BundleHandler.get("button.fetch"));
+        fetchRepoButton.setVisible(true);
+        fetchRepoButton.setWidth("20%");
+
+        dropDownLayout.addComponent(repoDropdown);
+        dropDownLayout.addComponent(fetchRepoButton);
+        dropDownLayout.setComponentAlignment(fetchRepoButton,Alignment.MIDDLE_LEFT);
+
+        dropDownLayout.setExpandRatio(repoDropdown,0.85f);
+        dropDownLayout.setExpandRatio(fetchRepoButton,0.75f);
+
+        fetchRepoButton.addClickListener(new FetchClickListener());
+
+        return dropDownLayout;
+    }
+
+    public TableHandler getTableHandler() {
+        return tableHandler;
+    }
+
+    public void setTableHandler(TableHandler tableHandler) {
+        this.tableHandler = tableHandler;
+    }
+
+    public DropdownHandler getDropdownHandler() {
+        return dropdownHandler;
+    }
+
+    public void setDropdownHandler(DropdownHandler dropdownHandler) {
+        this.dropdownHandler = dropdownHandler;
+    }
+}
